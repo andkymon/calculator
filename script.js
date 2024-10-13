d68da7381fcea0fb736fc82c7b522597b0dfee5b
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-13 19:02:44 +0800

    Prevent '.' from being clicked when decimal exists
    -remove '.' button from class 'num' so it does not get added an event
    handler along with other numbers
    -instead it is given an id 'dot', and a different function is executed
    when it is clicked
    -if display already has a decimal, the click event listener for the dot
    is removed.
    -else, '.' is appended to the display.
    -the #dot selector is grouped with the .num individual selectors where
    the button previously belonged.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ numberkeys = document.querySelectorAll(".num");
 
 clear = document.querySelector("#clear");
 backspace = document.querySelector("#backspace");
+dot = document.querySelector("#dot");
 
 addBtn = document.querySelector("#addBtn");
 subBtn = document.querySelector("#subBtn");
@@ -11,7 +12,7 @@ divBtn = document.querySelector("#divBtn");
 eqBtn = document.querySelector("#eqBtn");
 
 for (const number of numberkeys) {
-    number.addEventListener("click", function enablebtn(e) {
+    number.addEventListener("click", () => {
         if (display.textContent.length >= 10) {
             return;
         } else {
@@ -24,6 +25,15 @@ for (const number of numberkeys) {
 clear.addEventListener("click", () => display.textContent = "");
 backspace.addEventListener("click", () => {
     display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+});
+
+dot.addEventListener("click", function enabledot() {
+    if (display.textContent.includes(".")) {
+        dot.removeEventListener("click", enabledot);
+    } else {
+        display.textContent += ".";
+        console.log(display.textContent.length);
+    }
 });
 
 //Backspace
