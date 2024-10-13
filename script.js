6ddc3931330c36609a251c8f3009a813fc205fbe
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-13 20:35:01 +0800

    Add arithmetic operations buttons initial logic
    - Added functions for addition, subtraction, multiplication, and
    division.
    - Implemented event listeners for operation buttons.
    - Styled operation buttons when clicked, resetting style after input of
    the next number. This was done via another condition in the num click
    event listener, setting and resetting the operatorClicked flag.
    - Changed the dot button id from 'dot' to 'dotBtn'.

--- script.js
+++ script.js
@@ -5,28 +5,19 @@ clear = document.querySelector("#clear");
 backspace = document.querySelector("#backspace");
 dot = document.querySelector("#dot");
 
-addBtn = document.querySelector("#addBtn");
-subBtn = document.querySelector("#subBtn");
-mulBtn = document.querySelector("#mulBtn");
-divBtn = document.querySelector("#divBtn");
-eqBtn = document.querySelector("#eqBtn");
+operations = document.querySelectorAll(".operation");
+equal = document.querySelector("#eqBtn");
 
+//Function to append num or dot key content to sisplay
 function appendToDisplay(elem) {
     display.textContent += elem.textContent;
 }
-
-//Number Keys 
-for (const number of numberkeys) {
-    number.addEventListener("click", () => {
-        if (display.textContent.length >= 10) {
-            return;
-        } else {
-            appendToDisplay(number);
-        }
-    });
+//Function to clear display
+function clearDisplay() {
+    display.textContent = "";
 }
 //Clear Button
-clear.addEventListener("click", () => display.textContent = ""); 
+clear.addEventListener("click", clearDisplay); 
 //Backspace Button
 backspace.addEventListener("click", () => {
     display.textContent = display.textContent.slice(0, display.textContent.length - 1);
@@ -39,13 +30,81 @@ dot.addEventListener("click", function enabledot() {
         appendToDisplay(dot);
     }
 });
+//Operations Functions
+function add(num1, num2) {
+	return num1 + num2;
+};
 
+function subtract(num1, num2) {
+	return num1 - num2;
+};
 
+function multiply(num1, num2) {
+    return num1 * num2;
+};
 
-//add
-//subtract
-//multiply
-//divide
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+let num1;
+let oper;
+let num2;
+
+let operatorClicked;
+
+//Operation buttons
+for (const operation of operations) {
+    operation.addEventListener("click", () => {
+        //Set flag
+        operatorClicked = true;
+        //Set style while waiting for next click
+        operation.style.color = "orange";
+        operation.style.backgroundColor = "white";
+        //Save current display as num1 and clicked operation as oper
+        num1 = display.textContent;
+        oper = operation.textContent;
+    });
+}
+
+//Number Keys 
+for (const number of numberkeys) {
+    number.addEventListener("click", () => {
+        if (display.textContent.length >= 10) {
+            return;
+        } else if (operatorClicked === true) { //Condition if an operator button was clicked
+            operatorClicked = false; //Reset flag
+            //Reset operation button style
+            for (const operation of operations) {
+                operation.style.color = "";
+                operation.style.backgroundColor = "";
+            }
+            clearDisplay();
+            appendToDisplay(number);
+        } else {
+            appendToDisplay(number);
+        } 
+    });
+}
+//if operator is clicked: !!
+    //turn flag on!!
+    //change style !!
+    //store display content as num1 and button content as operator !!
+
+    //if num button is clicked: !!
+        //turn flag off !!
+        //revert style !!
+        //cleardisplay !!
+        //if equals is clicked:
+
+//if equals is clicked:
+    //turn flag off
+    //revert style
+    //store current display as num2
+    //operate on num1 and num2
+    //stop
+     
+
 //3 variables
 //operate
 
