3e9b0b8bec972ac160779e8eb00ce40cedd61452
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-13 17:49:57 +0800

    Implement 11-character limit for display content

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 display = document.querySelector("#display");
+
 numberkeys = document.querySelectorAll(".num");
 
-addBtn =
-subBtn =
-mulBtn =
-divBtn =
-eqBtn =
+addBtn = document.querySelector("#addBtn");
+subBtn = document.querySelector("#subBtn");
+mulBtn = document.querySelector("#mulBtn");
+divBtn = document.querySelector("#divBtn");
+eqBtn = document.querySelector("#eqBtn");
+
+clear = document.querySelector("#clear");
+backspace = document.querySelector("#backspace");
+
+for (const number of numberkeys) {
+    number.addEventListener("click", function enablebtn(e) {
+        if (display.textContent.length >= 11) {
+            for (const number of numberkeys) {
+                number.removeEventListener("click", enablebtn);
+            }
+        } else {
+            display.textContent += number.textContent;
+            console.log(display.textContent.length);
+        }
+    });
+}
+
+
+//Number Overflow disable on 11
